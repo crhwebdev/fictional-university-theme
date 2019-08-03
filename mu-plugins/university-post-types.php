f28c29a96a6c99d6f58a56ea39585731c9596178
Carl Hain <crhain@gmail.com>
2019-08-02 21:22:58 -0500

    Update custom Event type to show custom fly away label when hovering over the event post type

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -8,7 +8,8 @@
       'labels' => array(
         'name' => 'Events',
         'add_new_item' => 'Add New Event',
-        'edit_item' => 'Edit Event'
+        'edit_item' => 'Edit Event',
+        'all_items' => "All Events"
       ),
       'menu_icon' => 'dashicons-calendar'
     ));
