f31df6f0c4ac74d684e686c049cf8ffb6ae30f27
Carl Hain <crhain@gmail.com>
2019-08-06 16:20:50 -0500

    Update university-post-types.php to add archive support to events--currently does not work

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -4,13 +4,14 @@
     
     //register event post type
     register_post_type('event', array(
+      'has_archive' => true,
       'public' => true,
       'labels' => array(
         'name' => 'Events',
         'add_new_item' => 'Add New Event',
         'edit_item' => 'Edit Event',
         'all_items' => "All Events",
-        'singluar_name' => 'Event' 
+        'singluar_name' => 'Event'         
       ),
       'menu_icon' => 'dashicons-calendar'
     ));
--- themes/fictional-university-theme/single-event.php
+++ themes/fictional-university-theme/single-event.php
@@ -22,7 +22,7 @@
     
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p>
-        <a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>">
+        <a class="metabox__blog-home-link" href="<?php echo site_url('/event/') ?>">
           <i class="fa fa-home" aria-hidden="true"></i> Events Home
         </a> 
         <span class="metabox__main">Post by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span>
