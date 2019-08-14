25e9f59ecd78b61fa27582ba949a18be7d9806c2
Carl Hain <crhain@gmail.com>
2019-08-13 20:51:07 -0500

    Update university-post-types.php to enable custom fields for event posts

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -4,7 +4,7 @@
     
     //register event post type
     register_post_type('event', array(
-      'supports' => array('title', 'editor', 'excerpt'),
+      'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
       'rewrite' => array(
         'slug' => 'events'
       ),
